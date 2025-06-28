9e01869cce65df31917e5ecab05fb779866a65e9
r5ne <renetimantsev@gmail.com>
2025-06-28 12:52:31 +0200

    Made centralised event handler Created keybinds.py using the observer pattern

---
+++ src/core/events.py
@@ -0,0 +1,66 @@
+from src import data
+
+import pygame
+
+_keydown_keys = {}
+_keyup_keys = {}
+_held_keys = {}
+_mousedown_buttons = {}
+_mouseup_buttons = {}
+_held_buttons = {}
+mouse_pos = (0, 0)
+
+
+def is_key_down(key):
+    return key in _keydown_keys
+
+
+def is_key_up(key):
+    return key in _keydown_keys
+
+
+def is_key_held(key):
+    return key in _held_keys
+
+
+def is_mouse_down(button):
+    return button in _mousedown_buttons
+
+
+def is_mouse_up(button):
+    return button in _mouseup_buttons
+
+
+def is_mouse_held(button):
+    return button in _held_buttons
+
+
+def process_events(events):
+    global mouse_pos
+
+    _keydown_keys.clear()
+    _keyup_keys.clear()
+    _mousedown_buttons.clear()
+    _mouseup_buttons.clear()
+
+    for event in events:
+        match event.type:
+            case pygame.KEYDOWN:
+                _keydown_keys[event.key] = True
+                _held_keys[event.key] = True
+            case pygame.KEYUP:
+                _keyup_keys[event.key] = True
+                del _held_keys[event.key]
+            case pygame.MOUSEBUTTONDOWN:
+                _mousedown_buttons[event.button] = True
+                _held_buttons[event.button] = True
+            case pygame.MOUSEBUTTONUP:
+                _mouseup_buttons[event.button] = True
+                del _held_buttons[event.button]
+            case pygame.QUIT:
+                data.rom_data.running = False
+
+    _mouse_pos = tuple(
+        coord / data.rom_data.scale_factor[i]
+        for i, coord in enumerate(pygame.mouse.get_pos())
+    )
---
+++ src/core/keybinds.py
@@ -0,0 +1,66 @@
+import warnings
+from collections import defaultdict
+
+from src.core.events import (
+    is_key_down,
+    is_key_up,
+    is_key_held,
+    is_mouse_down,
+    is_mouse_up,
+    is_mouse_held,
+)
+from src.data import rom_data
+
+observers = defaultdict(list)
+input_checks = {
+    "keydown": is_key_down,
+    "keyup": is_key_up,
+    "key": is_key_held,
+    "mousebuttondown": is_mouse_down,
+    "mousebuttonup": is_mouse_up,
+    "mouse": is_mouse_held,
+    "quit": lambda: rom_data.running,
+}
+sorted_bindings_cache = []
+
+
+def register(*inputs, action):
+    observers[inputs].append(action)
+    sorted_bindings_cache.clear()
+
+
+def deregister(*inputs, action):
+    if action is not None and action in observers[inputs]:
+        observers[inputs].remove(action)
+        if not observers[inputs]:
+            del observers[inputs]
+    else:
+        warnings.warn(
+            f"Attempted to deregister inputs {inputs} that haven't"
+            f"been registered to the observers dict {observers}",
+            stacklevel=2,
+        )
+    sorted_bindings_cache.clear()
+
+
+def notify():
+    global sorted_bindings_cache
+    if not sorted_bindings_cache:
+        sorted_bindings_cache = sorted(
+            observers.items(), key=lambda binding: len(binding[0]), reverse=True
+        )
+    used_inputs = set()
+    for inputs, actions in sorted_bindings_cache:
+        for input_type, value in inputs:
+            check_func = input_checks.get(input_type)
+            if (
+                (input_type, value) not in used_inputs
+                and check_func(value)
+            ):
+                for action in actions:
+                    action()
+                used_inputs.update(inputs)
+
+
+def is_registered(*events, handler):
+    return events in observers and handler in observers[events]
