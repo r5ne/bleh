eccba523047962237256aafdd0c6f8ee88fd75f5
r5ne <renetimantsev@gmail.com>
2025-07-03 23:03:39 +0200

    Refactored bulletpool to preload bullets, and use constant time via a deque.

--- src/entities/bullets/bulletpool.py
+++ src/entities/bullets/bulletpool.py
@@ -1,20 +1,26 @@
+from collections import deque
+
 class BulletPool:
     def __init__(self, bullet_class, max_size, *init_args, **init_kwargs):
         self.bullet_class = bullet_class
-        self.pool = []
         self.max_size = max_size
         self.init_args = init_args
         self.init_kwargs = init_kwargs
+        self.pool = [self.bullet_class(*self.init_args, **self.init_kwargs) for _ in range(max_size)]
+        self.free = deque(self.pool)
+        self.in_use = deque()
 
     def aquire(self, *args, **kwargs):
-        if self.pool:
-            bullet = self.pool.pop()
-        else:
-            bullet = self.bullet_class(*self.init_args, **self.init_kwargs)
+        if not self.free:
+            oldest_bullet = self.in_use.popleft()
+            oldest_bullet.active = False
+            self.free.append(oldest_bullet)
+        bullet = self.free.pop()
         bullet.activate(*args, **kwargs)
+        self.in_use.append(bullet)
         return bullet
 
     def release(self, bullet):
-        if len(self.pool) < self.max_size:
-            bullet.deactivate()
-            self.pool.append(bullet)
+        bullet.active = False
+        self.in_use.remove(bullet)
+        self.free.append(bullet)
